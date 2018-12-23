d748d00026288f0195a4b271d0461438bab71f89
Yabosun <yabosun@163.com>
2018-12-23 20:42:49 +0800

    Solution 697

---
+++ leetcode/array/src/test/java/easy/leetcode697/SolutionOneTest.java
@@ -0,0 +1,15 @@
+package easy.leetcode697;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author YaboSun
+ */
+public class SolutionOneTest {
+
+    @Test
+    public void findShortestSubArray() {
+    }
+}

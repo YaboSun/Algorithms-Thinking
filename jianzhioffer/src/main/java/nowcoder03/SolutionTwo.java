a160c1b19a60e721b0a932a54fcee7833612f9b6
Yabosun <yabosun@163.com>
2019-01-05 20:29:25 +0800

    剑指Offer第三题 优化

---
+++ jianzhioffer/src/main/java/nowcoder03/SolutionTwo.java
@@ -0,0 +1,25 @@
+package nowcoder03;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+/**
+ * @author YaboSun
+ *
+ * 排行榜第一解法
+ */
+public class SolutionTwo {
+    public ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
+        Stack<Integer> stack = new Stack<>();
+        while (listNode != null) {
+            stack.push(listNode.val);
+            listNode = listNode.next;
+        }
+
+        ArrayList<Integer> list = new ArrayList<>();
+        while (!stack.isEmpty()) {
+            list.add(stack.pop());
+        }
+        return list;
+    }
+}

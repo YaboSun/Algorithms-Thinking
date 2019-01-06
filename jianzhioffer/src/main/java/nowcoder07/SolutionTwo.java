03cb959df6f7dad1da58e9f4f4b07502cb615697
Yabosun <yabosun@163.com>
2019-01-06 23:44:09 +0800

    剑指Offer第七题动态规划 非递归实现

---
+++ jianzhioffer/src/main/java/nowcoder07/SolutionTwo.java
@@ -0,0 +1,24 @@
+package nowcoder07;
+
+/**
+ * @author YaboSun
+ *
+ * 循环实现 不用递归
+ */
+public class SolutionTwo {
+    public int Fibonacci(int n) {
+        int a = 1, b = 1, c = 0;
+        if(n < 0) {
+            return 0;
+        } else if(n == 1 || n == 2) {
+            return 1;
+        } else {
+            for (int i = 3; i <= n; i++) {
+                c = a + b;
+                b = a;
+                a = c;
+            }
+            return c;
+        }
+    }
+}

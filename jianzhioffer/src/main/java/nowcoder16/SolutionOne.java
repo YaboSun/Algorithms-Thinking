53a8b2d6fa4d55484012f3f4bc20a6b23f253129
YaboSun <yabosun@163.com>
2019-01-09 20:40:12 +0800

    剑指offer16

---
+++ jianzhioffer/src/main/java/nowcoder16/SolutionOne.java
@@ -0,0 +1,30 @@
+package nowcoder16;
+
+/**
+ * @author YBSun
+ * Created in 2019-01-09
+ *
+ * 输入两个单调递增的链表，输出两个链表合成后的链表，
+ * 当然我们需要合成后的链表满足单调不减规则
+ *
+ * 递归进行比较
+ */
+public class SolutionOne {
+
+    public ListNode merge(ListNode list1, ListNode list2) {
+        if (list1 == null) {
+            return list2;
+        } else if (list2 == null) {
+            return list1;
+        }
+        ListNode mergeList = null;
+        if (list1.val < list2.val) {
+            mergeList = list1;
+            mergeList.next = merge(list1.next, list2);
+        } else {
+            mergeList = list2;
+            mergeList.next = merge(list1, list2.next);
+        }
+        return mergeList;
+    }
+}

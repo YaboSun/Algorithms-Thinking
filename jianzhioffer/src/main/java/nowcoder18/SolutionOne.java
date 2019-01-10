ecfe37bb497f24186ca4475d14f5606f11fd98f1
Yabosun <yabosun@163.com>
2019-01-10 23:22:02 +0800

    剑指Offer18

---
+++ jianzhioffer/src/main/java/nowcoder18/SolutionOne.java
@@ -0,0 +1,33 @@
+package nowcoder18;
+
+/**
+ * @author YaboSun
+ */
+public class SolutionOne {
+    public void Mirror(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        swap(root);
+        Mirror(root.left);
+        Mirror(root.right);
+    }
+
+    private void swap(TreeNode root) {
+        TreeNode t = root.left;
+        root.left = root.right;
+        root.right = t;
+    }
+
+    private class TreeNode {
+        int val = 0;
+        TreeNode left = null;
+        TreeNode right = null;
+
+        public TreeNode(int val) {
+            this.val = val;
+
+        }
+
+    }
+}

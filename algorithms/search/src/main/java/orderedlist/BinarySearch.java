b7ac1b8f0999018b50592d2d0efa2828facda348
Yabosun <yabosun@163.com>
2019-01-11 17:00:06 +0800

    折半查找优化

--- algorithms/search/src/main/java/orderedlist/BinarySearch.java
+++ algorithms/search/src/main/java/orderedlist/BinarySearch.java
@@ -6,13 +6,14 @@ package orderedlist;
 public class BinarySearch {
 
     /**
-     * 折半查找 非递归实现
+     * 折半查找 非递归实现 数组一定是有序的
      * @param arr 有序数组
      * @param n 数组最大下标
      * @param target 目标查找值
      * @return 返回目标值所在下标
      */
-    public int binarySearch(int[] arr, int n, int target) {
+    public int binarySearchOne(int[] arr, int n, int target) {
+        // 在arr[low...high]之中查找target
         int low = 1;
         int high = n;
         int mid;
@@ -28,5 +29,35 @@ public class BinarySearch {
             }
         }
         return 0;
+    }
+
+    /**
+     * 慕课网视频代码实现
+     * 在有序数组中查找target，如果找到返回相应索引的index
+     * 如果没找到返回-1
+     * @param arr 有序数组
+     * @param n 数组元素个数
+     * @param target 目标查找值
+     * @return 返回索引下标
+     */
+    public int binarySearchTwo(int[] arr, int n, int target) {
+        // 在arr[l...r]之中查找target
+        int l = 0;
+        int r = n - 1;
+
+
+        while (l <= r) {
+            // int mid = (l + r) / 2;
+            // 这个部分对于l r都是int型 可能出现相加溢出的情况,无bug版本如下
+            int mid = l + (r - l) / 2;
+            if (target == arr[mid]) {
+                return mid;
+            } else if (target > arr[mid]) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return -1;
     }
 }
--- algorithms/search/src/test/java/orderedlist/BinarySearchTest.java
+++ algorithms/search/src/test/java/orderedlist/BinarySearchTest.java
@@ -13,8 +13,8 @@ public class BinarySearchTest {
     @Test
     public void binarySearch() {
         int[] arr = {1, 2, 4, 5, 7, 8, 10, 12, 13};
-        int n = 8;
+        int n = 9;
         int target = 10;
-        System.out.println(binarySearch.binarySearch(arr, n, target));
+        System.out.println(binarySearch.binarySearchTwo(arr, n, target));
     }
 }

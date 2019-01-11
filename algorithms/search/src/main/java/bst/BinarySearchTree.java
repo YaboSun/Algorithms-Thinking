1d664d7362235c37b54ab293ac1ba9b5946397a2
Yabosun <yabosun@163.com>
2019-01-11 17:10:20 +0800

    二分搜索树初步构建

---
+++ algorithms/search/src/main/java/bst/BinarySearchTree.java
@@ -0,0 +1,37 @@
+package bst;
+
+/**
+ * @author YaboSun
+ */
+public class BinarySearchTree {
+
+    private Node root;
+    private int count;
+
+    public BinarySearchTree() {
+        root = null;
+        count = 0;
+    }
+
+    public int size() {
+        return count;
+    }
+
+    public boolean isEmpty() {
+        return count == 0;
+    }
+
+    private class Node {
+        String key;
+        int value;
+        Node left;
+        Node right;
+
+        public Node(String key, int value) {
+            this.key = key;
+            this.value = value;
+            this.left = null;
+            this.right = null;
+        }
+    }
+}

ec00069fc98b4c3b3083ad1121631370f6713696
YaboSun <yabosun@163.com>
2019-01-14 17:16:59 +0800

    leetcode347

--- leetcode/algorithm/src/main/java/sort/LeetCode347.java
+++ leetcode/algorithm/src/main/java/sort/LeetCode347.java
@@ -1,5 +1,7 @@
 package sort;
 
+import java.util.*;
+
 /**
  * @author YaboSun
  *
@@ -12,4 +14,70 @@ package sort;
  *
  */
 public class LeetCode347 {
+
+    /********************************************************
+     *                      个人代码初步实现
+     * ******************************************************/
+    /**
+     * 实现思路：利用一个HashMap存元素以及元素出现次数
+     * 将得到的map按照value进行排序
+     * 取得topK，输出对应的key存在一个ArrayList并返回
+     * @param nums
+     * @param k
+     * @return
+     */
+    public List<Integer> topKFrequent1(int[] nums, int k) {
+        if (nums.length == 0 || k <= 0) {
+            return null;
+        }
+        List<Integer> keys = new ArrayList<>();
+        Map<Integer, Integer> eleCountMap = new HashMap<>();
+        int count = 0;
+        // 构造map
+        for (int i = 0; i < nums.length; i++) {
+            if (!eleCountMap.containsKey(nums[i])) {
+                eleCountMap.put(nums[i], 1);
+            } else {
+                count = eleCountMap.get(nums[i]);
+                count++;
+                eleCountMap.put(nums[i], count);
+            }
+        }
+
+        // 取出value
+        return keys;
+    }
+
+    /********************************************************
+     *                      notebook代码
+     * ******************************************************/
+    public List<Integer> topKFrequent(int[] nums, int k) {
+
+        // 这里思路相同 构造的过程中别人很简单 学到了
+        Map<Integer, Integer> numFrequency = new HashMap<>();
+        for (int num : nums) {
+            // 这里的处理也很巧妙，直接通过foreach遍历
+            // 另外不用判断那么多 直接叠加
+            numFrequency.put(num, numFrequency.getOrDefault(num, 0) + 1);
+        }
+
+        // 这里还能创建这样的 第一次用都是
+        List<Integer>[] buckets = new ArrayList[nums.length + 1];
+        for (int key : numFrequency.keySet()) {
+            int frequency = numFrequency.get(key); // 这里的frequency对应的是元素出现的频率
+            if (buckets[frequency] == null) {
+                buckets[frequency] = new ArrayList<>(); // 对应的frequency创建一个新的ArrayList引用
+            }
+            buckets[frequency].add(key);
+        }
+
+        List<Integer> topK = new ArrayList<>();
+        for (int i = buckets.length - 1; i >= 0 && topK.size() < k; i--) {
+            if (buckets[i] != null) {
+                topK.addAll(buckets[i]);
+            }
+        }
+
+        return topK;
+    }
 }
---
+++ leetcode/algorithm/src/test/java/sort/LeetCode347Test.java
@@ -0,0 +1,16 @@
+package sort;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author YBSun
+ * Created in 2019-01-14
+ */
+public class LeetCode347Test {
+    public static void main(String[] args) {
+        LeetCode347 leetCode347 = new LeetCode347();
+        int[] nums = {1,1,1,2,2,3};
+        int k = 2;
+        leetCode347.topKFrequent(nums, k);
+    }
+}

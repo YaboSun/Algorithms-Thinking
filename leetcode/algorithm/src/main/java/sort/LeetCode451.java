261e93db6b667ed57b930b269113177a2b98cb96
YaboSun <yabosun@163.com>
2019-01-14 17:33:32 +0800

    leetcode347

---
+++ leetcode/algorithm/src/main/java/sort/LeetCode451.java
@@ -0,0 +1,22 @@
+package sort;
+
+/**
+ * @author YBSun
+ * Created in 2019-01-14
+ *
+ * Input:
+ * "tree"
+ *
+ * Output:
+ * "eert"
+ *
+ * Explanation:
+ * 'e' appears twice while 'r' and 't' both appear once.
+ * So 'e' must appear before both 'r' and 't'. Therefore "eetr" is also a valid answer.
+ * 题目描述：按照字符出现次数对字符串排序
+ */
+public class LeetCode451 {
+    public String frequencySort(String s) {
+        return " ";
+    }
+}

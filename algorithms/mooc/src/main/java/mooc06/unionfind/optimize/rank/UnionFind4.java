c927ef289882b4022afc9f5b7458117270544e52
Yabosun <yabosun@163.com>
2019-01-17 14:41:42 +0800

    union find第四种实现

---
+++ algorithms/mooc/src/main/java/mooc06/unionfind/optimize/rank/UnionFind4.java
@@ -0,0 +1,57 @@
+package mooc06.unionfind.optimize.rank;
+
+/**
+ * @author YaboSun
+ * 使用rank优化并查集
+ */
+public class UnionFind4 {
+
+    private int[] parent;
+    private int[] rank; // rank[i]表示以i为根的集合所表示的树的层数
+    private int count;
+
+    public UnionFind4(int count) {
+        parent = new int[count];
+        rank = new int[count];
+        this.count = count;
+
+        for (int i = 0; i < count; i++) {
+            parent[i] = i;
+            rank[i] = 1;
+        }
+    }
+
+    // 查找过程, 查找元素p所对应的集合编号
+    // O(h)复杂度, h为树的高度
+    private int find(int p){
+        assert( p >= 0 && p < count );
+        // 不断去查询自己的父亲节点, 直到到达根节点
+        // 根节点的特点: parent[p] == p
+        while( p != parent[p] )
+            p = parent[p];
+        return p;
+    }
+
+    // 查看元素p和元素q是否所属一个集合
+    // O(h)复杂度, h为树的高度
+    public boolean isConnected( int p , int q ){
+        return find(p) == find(q);
+    }
+
+    public void unionElements(int p, int q) {
+        int pRoot = find(p);
+        int qRoot = find(q);
+
+        if (pRoot == qRoot) {
+            return;
+        }
+        if (rank[pRoot] < rank[qRoot]) {
+            parent[pRoot] = qRoot;
+        } else if (rank[pRoot] > rank[qRoot]){
+            parent[qRoot] = pRoot;
+        } else {
+            parent[pRoot] = qRoot;
+            rank[qRoot] += 1;
+        }
+    }
+}
--- algorithms/mooc/src/test/java/mooc06/unionfind/UnionFindTest.java
+++ algorithms/mooc/src/test/java/mooc06/unionfind/UnionFindTest.java
@@ -16,5 +16,6 @@ public class UnionFindTest {
         UnionFindTestHelper.testUF1(n);
         UnionFindTestHelper.testUF2(n);
         UnionFindTestHelper.testUF3(n);
+        UnionFindTestHelper.testUF4(n);
     }
 }

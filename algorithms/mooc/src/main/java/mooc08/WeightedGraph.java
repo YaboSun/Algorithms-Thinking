e4d2ce624251ec9c2c1226565f38f65c5edf208a
Yabosun <yabosun@163.com>
2019-01-20 15:10:46 +0800

    提供接口 方便后面有权稠密图稀疏图实现

---
+++ algorithms/mooc/src/main/java/mooc08/WeightedGraph.java
@@ -0,0 +1,44 @@
+package mooc08;
+
+/**
+ * @author YaboSun
+ */
+interface WeightedGraph<Weight extends Number & Comparable> {
+
+    /**
+     * 获得顶点
+     * @return 对应的顶点
+     */
+    int V();
+
+    /**
+     * 获得对应的边
+     * @return 对应的边
+     */
+    int E();
+
+    /**
+     * 添加边
+     */
+    void addEdge(Edge<Weight> e);
+
+    /**
+     * 判断两节点是否有边连接
+     * @param v 节点v
+     * @param w 节点w
+     * @return 如果有边返回true
+     */
+    boolean hasEdge(int v, int w);
+
+    /**
+     * 打印有权图的信息
+     */
+    void show();
+
+    /**
+     * 返回节点v的所有邻边
+     * @param v 节点v
+     * @return 所有邻边
+     */
+    Iterable<Edge<Weight>> adj(int v);
+}

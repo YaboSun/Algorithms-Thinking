1e08d6ea6b48557971613551daac2d08b4820232
Yabosun <yabosun@163.com>
2019-01-20 21:29:11 +0800

    剑指offer 找到1-n中1的个数

---
+++ jianzhioffer/src/test/java/nowcoder31/SolutionTest.java
@@ -0,0 +1,14 @@
+package nowcoder31;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author YaboSun
+ */
+public class SolutionTest {
+    public static void main(String[] args) {
+        int n = 5;
+        Solution solution = new Solution();
+        System.out.println(solution.NumberOf1Between1AndN_Solution1(n));
+    }
+}

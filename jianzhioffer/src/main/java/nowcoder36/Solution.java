1e060778c439f7c064f73fc983cc4607f014b19a
Yabosun <yabosun@163.com>
2019-02-19 21:09:55 +0800

    nowcoder36 简化版

--- jianzhioffer/src/main/java/nowcoder36/Solution.java
+++ jianzhioffer/src/main/java/nowcoder36/Solution.java
@@ -2,9 +2,11 @@ package nowcoder36;
 
 /**
  * @author YaboSun
+ *
+ * 输入两个链表，找出它们的第一个公共结点
  */
 public class Solution {
-    public ListNode FindFirstCommonNode(ListNode pHead1, ListNode pHead2) {
+    public ListNode FindFirstCommonNode1(ListNode pHead1, ListNode pHead2) {
         if (pHead1 == null || pHead2 == null) {
             return null;
         }
@@ -12,7 +14,6 @@ public class Solution {
         // 计算两个链表的长度
         int count1 = 0; // 记录第一个链表长度
         int count2 = 0; // 记录第二个链表长度
-
         ListNode p1 = pHead1;
         ListNode p2 = pHead2;
         while (p1 != null) {
@@ -24,6 +25,7 @@ public class Solution {
             count2++;
         }
 
+        // 通过两链表长度差位置开始一起走
         int flag = count1 - count2;
         if (flag <= 0) {
             while (flag < 0) {
@@ -48,6 +50,18 @@ public class Solution {
         }
     }
 
+    // 上一种解法的简化版 思路类似
+    public ListNode FindFirstCommonNode2(ListNode pHead1, ListNode pHead2) {
+        ListNode p1 = pHead1;
+        ListNode p2 = pHead2;
+
+        while (p1 != p2) {
+            p1 = (p1 == null) ? pHead2 : p1.next;
+            p2 = (p2 == null) ? pHead1 : p2.next;
+        }
+
+        return p1;
+    }
     private class ListNode {
         int val;
         ListNode next = null;

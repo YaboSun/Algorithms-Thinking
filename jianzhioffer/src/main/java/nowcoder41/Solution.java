c3817d6e0a303a926ba9efaf7afa006a91612ecf
Yabosun <yabosun@163.com>
2019-03-08 09:26:07 +0800

    41

---
+++ jianzhioffer/src/main/java/nowcoder41/Solution.java
@@ -0,0 +1,36 @@
+package nowcoder41;
+
+import java.util.ArrayList;
+
+/**
+ * @author YaboSun
+ *
+ * 输入一个递增排序的数组和一个数字S，在数组中查找两个数，
+ * 使得他们的和正好是S，如果有多对数字的和等于S，
+ * 输出两个数的乘积最小的。
+ */
+public class Solution {
+
+    // 思路：使用双指针，第一个指针从头到尾进行遍历
+    // 第二个指针从尾到头进行遍历，如果tmp == sum,输出这两个数
+    // 如果tmp > sum,大指针减小，反之小指针增加
+    public ArrayList<Integer> FindNumbersWithSum(int [] array, int sum) {
+
+        ArrayList<Integer> list = new ArrayList<>();
+        if (array.length == 1) return list;
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = array.length - 1; j > 0; j--) {
+                int temp = array[i] + array[j];
+                if (temp == sum) {
+                    list.add(array[i]);
+                    list.add(array[j]);
+                    return list;
+                }
+                if (temp < sum) {
+                    break;
+                }
+            }
+        }
+        return list;
+    }
+}

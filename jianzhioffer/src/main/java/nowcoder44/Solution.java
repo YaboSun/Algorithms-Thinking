d5ad997fe9014a08f7c190d958393cf0d0d7ec02
Yabosun <yabosun@163.com>
2019-03-21 09:13:26 +0800

    nowcoder 44

--- jianzhioffer/src/main/java/nowcoder44/Solution.java
+++ jianzhioffer/src/main/java/nowcoder44/Solution.java
@@ -1,10 +1,32 @@
 package nowcoder44;
 
+import java.util.Arrays;
+
 /**
  * @author YaboSun
  */
 public class Solution {
     public boolean isContinuous(int [] numbers) {
+        if(numbers == null || numbers.length < 1) {
+            return false;
+        }
 
+        Arrays.sort(numbers);
+        int numOfZero = 0;
+        for(int i : numbers) {
+            if(i == 0)
+                ++numOfZero;
+        }
+        int numOfGap = 0;
+        int i = numOfZero;
+        int j = i + 1;
+        while(j < numbers.length) {
+            if(numbers[i] == numbers[j])
+                return false;
+            numOfGap += numbers[j] - numbers[i] - 1;
+            i = j;
+            ++j;
+        }
+        return numOfZero >= numOfGap;
     }
 }

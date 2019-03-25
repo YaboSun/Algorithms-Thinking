979038d7f99a40826b675ecb599eb542f75cadae
Yabosun <yabosun@163.com>
2019-03-25 20:19:04 +0800

    nowcoder 49

--- jianzhioffer/src/main/java/FirstDuplicate49.java
+++ jianzhioffer/src/main/java/FirstDuplicate49.java
@@ -30,4 +30,26 @@ public class FirstDuplicate49 {
         }
         return duplication[0] == ret;
     }
+
+    public boolean duplicate2(int numbers[], int length, int [] duplication) {
+
+        if (numbers == null || length < 1)
+            return false;
+        for (int i = 0; i < length; i++) {
+            while (numbers[i] != i) {
+                if (numbers[i] == numbers[numbers[i]]) {
+                    duplication[0] = numbers[i];
+                    return true;
+                }
+                swap(numbers, i, numbers[i]);
+            }
+        }
+        return false;
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
 }

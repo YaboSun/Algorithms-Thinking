c26107dc70f167e5d881b436ea1c67ac9ac176b3
Yabosun <yabosun@163.com>
2019-04-08 09:33:08 +0800

    notes-01

---
+++ csbasic/src/test/java/notes/Duplicate01Test.java
@@ -0,0 +1,14 @@
+package notes;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author YaboSun
+ */
+public class Duplicate01Test {
+    public static void main(String[] args) {
+        Duplicate01 duplicate01 = new Duplicate01();
+        int[] nums = {2,3,1,0,5,5,3,2,1,0};
+        duplicate01.duplicate(nums);
+    }
+}

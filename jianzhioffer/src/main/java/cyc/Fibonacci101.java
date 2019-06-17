90aec067b2c37800c7abfd125b0595d51c3f751b
yabosun <yabosun@163.com>
2019-06-17 10:04:55 +0800

    jz-fib

--- jianzhioffer/src/main/java/cyc/Fibonacci101.java
+++ jianzhioffer/src/main/java/cyc/Fibonacci101.java
@@ -10,6 +10,7 @@ public class Fibonacci101 {
 
     /**
      * 递归解法
+     * 通过开辟一个n+1大小的数组，对应第n项值
      * @param n
      * @return
      */
@@ -24,6 +25,29 @@ public class Fibonacci101 {
         }
 
         return fib[n];
+    }
+
+    /**
+     * 优化上一解法的空间复杂度
+     * 因为计算第n项，只需要知道前两项即可
+     * @param n
+     * @return
+     */
+    public int Fibonacci1(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        int pre1 = 1;
+        int pre2 = 0;
+        int fib = 0;
+
+        for (int i = 2; i <= n; i++) {
+            fib = pre1 + pre2;
+            pre2 = pre1;
+            pre1 = fib;
+        }
+
+        return fib;
     }
 }
 
